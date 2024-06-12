602d08e80ad28d03d2bfdc80305b4c79306ec906
GOGOSCHNO <157826855+GOGOSCHNO@users.noreply.github.com>
2024-06-11 20:22:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function sendUserInput() {
 
     const typingIndicator = showTypingIndicator();
 
-    fetch('https://ancient-island-80614-679de07529b5.herokuapp.com/send_message', {
+    fetch('https://javaburn.herokuapp.com/send_message', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
